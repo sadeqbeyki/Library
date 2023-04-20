7b22eec7a5e7ecb0dfa326fb7d2ff6b91d02a0af
mardibak <mardibaki@gmail.com>
2023-04-21 02:02:53 +0330

    updated framework

--- AppFramework/Application/ApplicationMessages.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AppFramework.Application
-{
-    public class ApplicationMessages
-    {
-        public const string DuplicatedRecord = "امکان ثبت رکورد تکراری وجود ندارد";
-        public const string RecordNotFound = "رکوردی یافت نشد";
-    }
-}
