a569cddd26b7413581136f6124b80aa4e5d3e335
sadeqbeyki <mardibaki@gmail.com>
2023-05-19 00:30:06 +0330

    translator controller implemented

---
+++ LMS.EndPoint/Controllers/TranslatorController.cs
@@ -0,0 +1,62 @@
+using LMS.Contracts.Translator;
+using LMS.Domain.TranslatorAgg;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.EndPoint.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TranslatorController : ControllerBase
+    {
+        private readonly ITranslatorService _translatorService;
+
+        public TranslatorController(ITranslatorService translatorService)
+        {
+            _translatorService = translatorService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllTranslators()
+        {
+            var translators = _translatorService.GetAllTranslators();
+            return Ok(translators);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTranslatorById(Guid id)
+        {
+            var translator = await _translatorService.GetTranslator(id);
+            if (translator == null)
+                return NotFound();
+
+            return Ok(translator);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTranslator([FromBody] TranslatorDto dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var result = await _translatorService.AddTranslator(dto);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<TranslatorDto> UpdateTranslator(Guid id, [FromBody] TranslatorDto translator)
+        {
+            //if (!ModelState.IsValid)
+            //    return BadRequest(ModelState);
+            var updatedTranslator = await _translatorService.UpdateTranslator(id, translator);
+
+            return updatedTranslator;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTranslator(Guid id)
+        {
+            await _translatorService.DeleteTranslator(id);
+            return NoContent();
+        }
+    }
+
+}
---
+++ LMS.EndPoint/Program.cs
@@ -0,0 +1,41 @@
+using LMS.Contracts.Translator;
+using LMS.Domain.TranslatorAgg;
+using LMS.Infrastructure;
+using LMS.Infrastructure.Repositories;
+using LMS.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<BookDbContext>(opt=>opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddScoped<IBookCategoryRepository, BookCategoryRepository>();
+//builder.Services.AddScoped<IPublisherRepository, PublisherRepository>();
+//builder.Services.AddScoped<IAutherRepository, AutherRepository>();
+builder.Services.AddScoped<ITranslatorRepository, TranslatorRepository>();
+builder.Services.AddScoped<ITranslatorService, TranslatorService>();
+
+//builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
