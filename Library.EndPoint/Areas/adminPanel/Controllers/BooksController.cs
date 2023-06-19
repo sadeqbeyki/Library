ea0e793eb00d9586e3a8f9b65624b3822d9b5bed
mardibak <mardibaki@gmail.com>
2023-06-19 23:05:57 +0330

    get all book action implemented

---
+++ Library.EndPoint/Areas/adminPanel/Controllers/BooksController.cs
@@ -0,0 +1,22 @@
+﻿using LMS.Contracts.Book;
+using LMS.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.EndPoint.Areas.adminPanel.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly BookService _bookService;
+
+        public BooksController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        public async Task<ActionResult<List<BookViewModel>>> Index()
+        {
+            var result = await _bookService.GetAll();
+            return View(result);
+        }
+    }
+}
