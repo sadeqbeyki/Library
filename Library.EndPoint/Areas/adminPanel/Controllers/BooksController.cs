ec33b7e3c27fe69dfeeae5ccf5587c17ed802b7b
mardibak <mardibaki@gmail.com>
2023-06-20 23:33:17 +0330

    get books updated

--- Library.EndPoint/Areas/adminPanel/Controllers/BooksController.cs
+++ Library.EndPoint/Areas/adminPanel/Controllers/BooksController.cs
@@ -1,21 +1,29 @@
 ﻿using LMS.Contracts.Book;
-using LMS.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Library.EndPoint.Areas.adminPanel.Controllers
 {
+    [Area("adminPanel")]
     public class BooksController : Controller
     {
-        private readonly BookService _bookService;
+        private readonly IBookService _bookService;
 
-        public BooksController(BookService bookService)
+        public BooksController(IBookService bookService)
         {
             _bookService = bookService;
         }
-
+        [HttpGet]
         public async Task<ActionResult<List<BookViewModel>>> Index()
         {
             var result = await _bookService.GetAll();
+            return View(result);
+        }
+        [HttpGet]
+        public async Task<ActionResult<BookViewModel>> Details(Guid id)
+        {
+            var result = await _bookService.GetById(id);
+            if (result == null)
+                return NotFound();
             return View(result);
         }
     }
