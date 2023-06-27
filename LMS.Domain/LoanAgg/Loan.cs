4940a2bf736583c945bf04f876c515e6a9062092
sadeqbeyki <mardibaki@gmail.com>
2023-06-27 19:39:32 +0330

    entities updated

---
+++ LMS.Domain/LoanAgg/Loan.cs
@@ -0,0 +1,13 @@
+﻿namespace LMS.Domain.LoanAgg;
+
+public class Loan
+{
+    public  int Id { get; set; }
+    public Guid BookId { get; set; }
+    public string EmployeeId { get; set; }
+    public string LoanDate { get; set; }
+    public string IdealReturnDate { get; set;}
+    public string ReturnEmployeeID { get; set;}
+    public string ReturnDate { get; set;}
+    public string Description { get; set;}
+}
