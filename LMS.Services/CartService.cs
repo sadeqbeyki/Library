4e7b444eb2d2d0a0d760f1bc7b441b87ee8c82bc
sadeqbeyki <mardibaki@gmail.com>
2023-07-01 20:09:11 +0330

    borrow book updated

---
+++ LMS.Services/CartService.cs
@@ -0,0 +1,20 @@
+﻿
+using LMS.Contracts.Rent;
+
+namespace LMS.Services
+{
+    public class CartService : ICartService
+    {
+        public Cart Cart { get; set; }
+
+        public Cart Get()
+        {
+            return Cart;
+        }
+
+        public void Set(Cart cart)
+        {
+            Cart = cart;
+        }
+    }
+}
