2087f3daa51eda1de94c384ee734ae5a1be38624
sadeqbeyki <mardibaki@gmail.com>
2023-07-13 23:56:47 +0330

    getAllLoans Action Implemented

---
+++ Library.EndPoint/Areas/adminPanel/Controllers/LoansController.cs
@@ -0,0 +1,20 @@
+﻿using LMS.Contracts.Loan;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.EndPoint.Areas.adminPanel.Controllers;
+[Area("adminPanel")]
+public class LoansController : Controller
+{
+    private readonly ILoanService _loanService;
+
+    public LoansController(ILoanService loanService)
+    {
+        _loanService = loanService;
+    }
+   
+    public async Task<ActionResult<List<LoanDto>>> Index()
+    {
+        List<LoanDto> loans = await _loanService.GetAllLoans();
+        return View("Index", loans);
+    }
+}
