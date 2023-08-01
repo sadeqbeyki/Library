7d7e3d24cbdfdd5bfbc82a3723948cbdde0bbac0
mardibak <mardibaki@gmail.com>
2023-08-01 21:10:19 +0330

    verfication updated in ui

--- LibInv.DomainContracts/Inventory/DecreaseInventory.cs
+++ LibInv.DomainContracts/Inventory/DecreaseInventory.cs
@@ -1,10 +1,14 @@
-﻿namespace LibInventory.DomainContracts.Inventory;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibInventory.DomainContracts.Inventory;
 
 public class DecreaseInventory
 {
     public int InventoryId { get; set; }
     public int BookId { get; set; }
+    [Required(ErrorMessage = "Number field is required.")]
     public long Count { get; set; }
+    [Required(ErrorMessage = "Description field is required.")]
     public string Description { get; set; }
     public int LendId { get; set; }
 
