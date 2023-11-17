343f54370cd8a67c9846ddc34e020e465a58a834
mardibak <mardibaki@gmail.com>
2023-11-17 19:31:00 +0330

    change borrowDate To Creation date into loanDto class

--- LibBook.DomainContracts/Borrow/LoanDto.cs
+++ LibBook.DomainContracts/Borrow/LoanDto.cs
@@ -10,7 +10,7 @@ public class LoanDto
     [Required(ErrorMessage = "Username field cannot be empty!")]
     public string MemberId { get; set; }
     public string EmployeeId { get; set; }
-    public DateTime BorrowDate { get; set; }
+    public DateTime CreationDate { get; set; }
     public DateTime IdealReturnDate { get; set; }
     public string ReturnEmployeeId { get; set; }
     public DateTime? ReturnDate { get; set; }
