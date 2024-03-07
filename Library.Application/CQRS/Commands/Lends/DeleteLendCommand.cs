c8fc35badb3ef8f5c0d5dffe812d76e778e53f66
mardibak <mardibaki@gmail.com>
2024-03-08 00:26:14 +0330

    delete and soft delete Commands implemented

---
+++ Library.Application/CQRS/Commands/Lends/DeleteLendCommand.cs
@@ -0,0 +1,22 @@
+﻿using Library.Application.Interfaces;
+using MediatR;
+
+namespace Library.Application.CQRS.Commands.Lends;
+
+public record DeleteLendCommand(int lendId) : IRequest
+{
+}
+internal sealed class DeleteLendCommandHandler : IRequestHandler<DeleteLendCommand>
+{
+    private readonly ILendRepository _lendRepository;
+    public DeleteLendCommandHandler(ILendRepository lendRepository)
+    {
+        _lendRepository = lendRepository;
+    }
+
+    public async Task Handle(DeleteLendCommand request, CancellationToken cancellationToken)
+    {
+        var lend = await _lendRepository.GetByIdAsync(request.lendId);
+        await _lendRepository.DeleteAsync(lend);
+    }
+}
---
+++ Library.Application/CQRS/Commands/Lends/SoftDeleteLendCommand.cs
@@ -0,0 +1,24 @@
+﻿using Library.Application.DTOs.Lend;
+using Library.Application.Interfaces;
+using Library.Domain.Entities.LendAgg;
+using MediatR;
+
+namespace Library.Application.CQRS.Commands.Lends;
+
+public record SoftDeleteLendCommand(LendDto Dto) : IRequest
+{
+}
+internal sealed class SoftDeleteLendCommandHandler : IRequestHandler<SoftDeleteLendCommand>
+{
+    private readonly ILendRepository _lendRepository;
+    public SoftDeleteLendCommandHandler(ILendRepository lendRepository)
+    {
+        _lendRepository = lendRepository;
+    }
+
+    public async Task Handle(SoftDeleteLendCommand request, CancellationToken cancellationToken)
+    {
+        Lend lend = _lendRepository.GetByIdAsync(request.Dto.Id).Result;
+        _lendRepository.SoftDelete(lend);
+    }
+}
