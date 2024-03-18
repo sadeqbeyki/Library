e85b0d337bb61319ae51834580c9992383cec565
sadeq.beyki <mardibaki@gmail.com>
2024-03-18 11:14:39 +0330

    add publisherUpdate action

--- Library.EndPoint.MVC/Areas/adminPanel/Controllers/PublishersController.cs
+++ Library.EndPoint.MVC/Areas/adminPanel/Controllers/PublishersController.cs
@@ -1,6 +1,7 @@
 ﻿using Library.Application.CQRS.Commands.Authors;
 using Library.Application.CQRS.Commands.Publishers;
 using Library.Application.CQRS.Queries.Publishers;
+using Library.Application.DTOs.Authors;
 using Library.Application.DTOs.Publishers;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -43,5 +44,21 @@ public class PublishersController : Controller
         }
         var result = await _mediator.Send(new CreatePublisherCommand(model));
         return RedirectToAction("Index");
+    }
+    [HttpGet]
+    public async Task<ActionResult<AuthorDto>> Update(int id)
+    {
+        var result = await _mediator.Send(new GetPublisherQuery(id));
+        return View("Update", result);
+    }
+    [HttpPost]
+    public async Task<ActionResult> Update(PublisherDto model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View();
+        }
+        var result = await _mediator.Send(new UpdatePublisherCommand(model));
+        return RedirectToAction("Index", result);
     }
 }
